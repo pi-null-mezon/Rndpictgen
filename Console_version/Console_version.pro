fde64f084fa751f305ffac15aa5430737b3a9d5a
Alex Taranov <pi-null-mezon@yandex.ru>
2015-09-04 00:44:01 +0400

    First release built

--- Console_version/Console_version.pro
+++ Console_version/Console_version.pro
@@ -10,8 +10,16 @@ QT       += core \
 CONFIG   += console
 CONFIG   -= app_bundle
 
-TARGET = RPGConsole
+TARGET = randpicture
+VERSION =   1.0.0.0
+
+DEFINES +=  APP_NAME=\\\"$${TARGET}\\\" \
+            APP_VERS=\\\"$${VERSION}\\\" \
+            APP_DESIGNER=\\\"Alex.A.Taranov\\\"
 
 TEMPLATE = app
 
 SOURCES += main.cpp
+
+
+CONFIG(release, debug|release): DEFINES += QT_NO_WARNING_OUTPUT
--- Console_version/main.cpp
+++ Console_version/main.cpp
@@ -3,39 +3,46 @@
 #include <QPainter>
 #include <QPainterPath>
 #include <windows.h>
+#include <cstdio>
+//---------------------------------------------------------------------------------
 
 QColor qrandcolor();
 qreal qfrand();
-QImage randompaint(const QImage& input, uint triangles, uint ellipses, uint rectangles, uint circles, uint fract, uint maxiter);
+void randomPaint(QImage& input, uint triangles, uint ellipses, uint rectangles, uint circles, uint sizedivisor, bool contour);
 void convertToGrayscale(QImage &input);
+void applySaltPapper(QImage &input);
+void applyNoise(QImage &input);
+enum Primitives {RectanglePrimitive, TrianglePrimitive, EllipsePrimitive, CirclePrimitive};
 
+//---------------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
-    qsrand(GetTickCount());
+    qsrand(GetTickCount()); // to seed pseudo-random number generator
     int m_exitCode;
-    char m_cmdOption;
     QString m_fileName("Default.png");
-    int m_pictureWidth = 512;
-    int m_pictureHeight = 512;
+    int m_pictureWidth = 0;
+    int m_pictureHeight = 0;
     uint m_rectNumber = 0;
     uint m_ellipseNumber = 0;
     uint m_triangleNumber = 0;
     uint m_circleNumber = 0;
-    uint m_iterationNumber = 128;
-    uint m_objectSize = 32;
+    uint m_sizeDivisor = 0;
     bool f_grayscale = false;
+    bool f_saltpepper = false;
+    bool f_noise = false;
+    bool f_contour = false;
 
     while( (--argc > 0) && ((*++argv)[0] == '-') ) { // a loop till argv[] ends
-        m_cmdOption = *++argv[0];
+        char m_cmdOption = *++argv[0];
         switch (m_cmdOption)
         {
             case 'o':
                 m_fileName = QString(++(*argv));
                 break;
-            case 'h':
+            case 'y':
                 m_pictureHeight = QString(++(*argv)).toInt();
                 break;
-            case 'w':
+            case 'x':
                 m_pictureWidth = QString(++(*argv)).toInt();
                 break;
             case 't':
@@ -50,26 +57,54 @@ int main(int argc, char *argv[])
             case 'c':
                 m_circleNumber = QString(++(*argv)).toUInt();
                 break;
-            case 'i':
-                m_iterationNumber = QString(++(*argv)).toUInt();
-                break;
-            case 's':
-                m_objectSize = QString(++(*argv)).toUInt();
+            case 'd':
+                m_sizeDivisor = QString(++(*argv)).toUInt();
                 break;
             case 'g':
                 f_grayscale = true;
                 break;
+            case 's':
+                f_saltpepper = true;
+                break;
+            case 'n':
+                f_noise = true;
+                break;
+            case 'b':
+                f_contour = true;
+                break;
+            case 'h':
+                std::printf("%s v.%s, designed by %s\n"
+                            " -h - force to show this help"
+                            " -o[Ouput file name]\n"
+                            " -y[Height] - desired picture height\n"
+                            " -x[Width] - desired picture width\n"
+                            " -t[Triangles] - desired quantity of primitives\n"
+                            " -c[Circles] - desired quantity of primitives\n"
+                            " -r[Rectangles] - desired quantity of primitives\n"
+                            " -e[Ellipses] - desired quantity of primitives\n"
+                            " -d[Divider] - smaller divider provides larger primitives\n"
+                            " -n - force to apply additive noise\n"
+                            " -s - force to apply salt and pepper interference\n"
+                            " -g - force to generate grayscale picture\n"
+                            " -b - force to add border line to primitives\n"
+                            "Sources are available at https://github.com/pi-null-mezon/randpicture.git\n"
+                            , QString(APP_NAME).toLocal8Bit().constData()
+                            , QString(APP_VERS).toLocal8Bit().constData()
+                            , QString(APP_DESIGNER).toLocal8Bit().constData());
+                break;
         }
     }
-    if((m_pictureHeight > 0) && (m_pictureWidth > 0) && (m_objectSize > 0)) {
-        QImage m_picture(m_pictureHeight, m_pictureWidth, QImage::Format_RGB888);
-        m_picture = randompaint(m_picture, m_triangleNumber, m_ellipseNumber, m_rectNumber, m_circleNumber, m_objectSize, m_iterationNumber);
-        if(f_grayscale) {
+    if((m_pictureHeight > 0) && (m_pictureWidth > 0) && (m_sizeDivisor > 0)) {
+        QImage m_picture(m_pictureWidth, m_pictureHeight,  QImage::Format_RGB888);
+        randomPaint(m_picture, m_triangleNumber, m_ellipseNumber, m_rectNumber, m_circleNumber, m_sizeDivisor, f_contour);
+        if(f_saltpepper)
+            applySaltPapper(m_picture);
+        if(f_noise)
+            applyNoise(m_picture);
+        if(f_grayscale)
             convertToGrayscale(m_picture);
-        }
         if(m_picture.save(m_fileName)) {
-            qWarning("Picture has been saved to a file: %s", m_fileName.toLocal8Bit().constData());
-            qWarning("Image dimensions: height %d x width %d", m_pictureHeight, m_pictureWidth);
+            std::printf("Picture has been saved to a file: %s\n", m_fileName.toLocal8Bit().constData());
             m_exitCode = 0;
         } else {
             m_exitCode = -2;
@@ -77,161 +112,114 @@ int main(int argc, char *argv[])
     } else {
         m_exitCode = -1;
     }
-
     return m_exitCode;
 }
+//---------------------------------------------------------------------------------
 
+//---------------------------------------------------------------------------------
 QColor qrandcolor()
 {
     return QColor(qrand() % 256, qrand() % 256, qrand() % 256, qrand() % 256);
 }
+//---------------------------------------------------------------------------------
 
+//---------------------------------------------------------------------------------
 qreal qfrand()
 {
     return ((qreal)qrand() / (RAND_MAX + 1.0));
 }
+//---------------------------------------------------------------------------------
 
-QRectF randrect(qreal width, qreal height, qreal fraction)
+//---------------------------------------------------------------------------------
+QRectF randrect(qreal width, qreal height, qreal divisor)
 {
     qreal x = ((2.0 * qfrand() - 1.0) * width/2.5);
     qreal y = ((2.0 * qfrand() - 1.0) * height/2.5);
-    qreal w = ((qfrand() + 1.0) * width/fraction);
-    qreal h = ((qfrand() + 1.0) * height/fraction);
+    qreal w = ((qfrand() + 1.0) * width/divisor);
+    qreal h = ((qfrand() + 1.0) * height/divisor);
     return QRectF(x,y,w,h);
 }
-QRectF randsquare(qreal width, qreal height, qreal fraction)
+//---------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------
+QRectF randsquare(qreal width, qreal height, qreal divisor)
 {
     qreal x = ((2.0 * qfrand() - 1.0) * width/2.5);
     qreal y = ((2.0 * qfrand() - 1.0) * height/2.5);
-    qreal d = ((qfrand() + 1.0) * (width + height)/(2.5*fraction));
+    qreal d = ((qfrand() + 1.0) * (width + height)/(2.5*divisor));
     return QRectF(x,y,d,d);
 }
+//---------------------------------------------------------------------------------
 
-QImage randompaint(const QImage& input, uint triangles, uint ellipses, uint rectangles, uint circles, uint fract, uint maxiter)
+//---------------------------------------------------------------------------------
+void randomPaint(QImage& input, uint triangles, uint ellipses, uint rectangles, uint circles, uint sizedivisor, bool contour)
 {
-    int steps = maxiter;
-    QImage output(input);
-    QPainter painter(&output);
+    QPainter painter(&input);
     painter.setRenderHint(QPainter::Antialiasing);
-    painter.setPen(Qt::NoPen);
-
-    painter.fillRect(output.rect(), qrandcolor());
+    if(!contour)
+        painter.setPen(Qt::NoPen);
+    else
+        painter.setPen(Qt::SolidLine);
+    painter.fillRect(input.rect(), qrandcolor());
     painter.translate(input.width() / 2.0, input.height() / 2.0);
 
     QList<QRectF> regions;
-    QRectF newRect;
-    bool intersection;
-    {
-        for(uint i = 0; i < rectangles; i++)
+    QRectF rect;
+    QPolygonF polygon;
+    uint cycles;
+    for(int key = 0; key <= 3; key++) {
+        switch(key) {
+        case RectanglePrimitive:
+            cycles = rectangles;
+            break;
+        case TrianglePrimitive:
+            cycles = triangles;
+            break;
+        case CirclePrimitive:
+            cycles = circles;
+            break;
+        case EllipsePrimitive:
+            cycles = ellipses;
+            break;
+        }
+        for(uint i = 0; i < cycles; i++)
         {
-            do {
-                intersection = false;
-                newRect = randrect(input.width(), input.height(), (qreal)fract);
-                steps--;
-                qWarning("Available iterations remain: %d", steps);
-                foreach(const QRectF& oldRect, regions) {
-                    if(oldRect.intersects(newRect)) {
-                        intersection = true;
-                        break;
-                    }
-                }
-            } while( (intersection == true) && (steps > 0) );
-            regions.append(newRect);
-
+            if(key == CirclePrimitive) {
+                rect = randsquare(input.width(), input.height(), (qreal)sizedivisor);
+            } else {
+                rect = randrect(input.width(), input.height(), (qreal)sizedivisor);
+            }
             QPainterPath path;
-            path.addRect(newRect);
+            switch(key) {
+            case RectanglePrimitive:
+                path.addRect(rect);;
+                break;
+            case TrianglePrimitive:
+                polygon.clear();
+                polygon << QPointF((rect.topLeft()+rect.topRight())/2.0)
+                        << QPointF(rect.bottomLeft())
+                        << QPointF(rect.bottomRight())
+                        << QPointF((rect.topLeft()+rect.topRight())/2.0);
+                path.addPolygon(polygon);
+                break;
+            case CirclePrimitive:
+                path.addEllipse(rect);
+                break;
+            case EllipsePrimitive:
+                path.addEllipse(rect);
+                break;
+            }
             painter.setBrush(qrandcolor());
-
             painter.save();
             painter.rotate(360.0 * qfrand());
             painter.drawPath(path);
             painter.restore();
         }
     }
-    for(uint i = 0; i < triangles; i++)
-    {
-        do {
-            intersection = false;
-            newRect = randrect(input.width(), input.height(), (qreal)fract);
-            steps--;
-            qWarning("Available iterations remain: %d", steps);
-            foreach(const QRectF& oldRect, regions) {
-                if(oldRect.intersects(newRect)) {
-                    intersection = true;
-                    break;
-                }
-            }
-        } while((intersection == true) && (steps > 0));
-        regions.append(newRect);
-
-        QPointF p1((newRect.topLeft()+newRect.topRight())/2.0);
-        QPointF p2(newRect.bottomLeft());
-        QPointF p3(newRect.bottomRight());
-        QPolygonF triangle;
-        triangle << p1 << p2 << p3;
-
-        QPainterPath path;
-        path.addPolygon(triangle);
-        painter.setBrush(qrandcolor());
-
-        painter.save();
-        painter.rotate(360.0 * qfrand());
-        painter.drawPath(path);
-        painter.restore();
-    }
-    for(uint i = 0; i < ellipses; i++)
-    {
-        do {
-            intersection = false;
-            newRect = randrect(input.width(), input.height(), (qreal)fract);
-            steps--;
-            qWarning("Available iterations remain: %d", steps);
-            foreach(const QRectF& oldRect, regions) {
-                if(oldRect.intersects(newRect)) {
-                    intersection = true;
-                    break;
-                }
-            }
-        } while((intersection == true) && (steps > 0));
-        regions.append(newRect);
-
-        QPainterPath path;
-        path.addEllipse(newRect);
-        painter.setBrush(qrandcolor());
-
-        painter.save();
-        painter.rotate(360.0 * qfrand());
-        painter.drawPath(path);
-        painter.restore();
-    }
-    for(uint i = 0; i < circles; i++)
-    {
-        do {
-            intersection = false;
-            newRect = randsquare(input.width(), input.height(), (qreal)fract);
-            steps--;
-            qWarning("Available iterations remain: %d", steps);
-            foreach(const QRectF& oldRect, regions) {
-                if(oldRect.intersects(newRect)) {
-                    intersection = true;
-                    break;
-                }
-            }
-        } while((intersection == true) && (steps > 0));
-        regions.append(newRect);
-
-        QPainterPath path;
-        path.addEllipse(newRect);
-        painter.setBrush(qrandcolor());
-
-        painter.save();
-        painter.rotate(360.0 * qfrand());
-        painter.drawPath(path);
-        painter.restore();
-    }
-    return output;
 }
+//---------------------------------------------------------------------------------
 
+//---------------------------------------------------------------------------------
 void convertToGrayscale(QImage &input)
 {
     uchar *pt;
@@ -248,6 +236,52 @@ void convertToGrayscale(QImage &input)
         }
     }
 }
+//---------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------
+void applySaltPapper(QImage &input)
+{
+    uchar *pt;
+    int dice;
+    for(int i = 0; i < input.height(); i++) {
+        pt = input.scanLine(i);
+        for(int j = 0; j < input.width(); j++) {
+            dice = qrand();
+            if((dice % 53) == 0) {
+                pt[3*j] = 0;
+                pt[3*j+1] = 0;
+                pt[3*j+2] = 0;
+            } else if((dice % 59) == 0) {
+                pt[3*j] = 255;
+                pt[3*j+1] = 255;
+                pt[3*j+2] = 255;
+            }
+        }
+    }
+}
+//---------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------
+void applyNoise(QImage &input)
+{
+    uchar *pt;
+    int dice;
+    int pos;
+    int temp;
+    for(int i = 0; i < input.height(); i++) {
+        pt = input.scanLine(i);
+        for(int j = 0; j < input.width(); j++) {
+            pos = 3*j;
+            dice = qrand() % 37;
+            temp = (int)pt[pos] + dice;
+            pt[pos] = (temp > 255) ? 255 : temp;
+            temp = (int)pt[pos+1] + dice;
+            pt[pos+1] = (temp > 255) ? 255 : temp;
+            temp = (int)pt[pos+2] + dice;
+            pt[pos+2] = (temp > 255) ? 255 : temp;
+        }
+    }
+}
+//---------------------------------------------------------------------------------
 
 
 
